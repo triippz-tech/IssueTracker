b249a5bc8554fb226befdd202a6f13040033d1ae
Mark Tripoli <marktripoli@marks-mbp.amtrak.ad.nrpc>
2019-06-03 11:22:14 -0400

    Initial application generated by JHipster-6.0.1

---
+++ src/main/java/com/triippztech/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.triippztech.repository;
+
+import com.triippztech.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
